cbe8542fad1466c2032bdc5e62ea6cf7753eec18
EC2 Default User <ec2-user@ip-172-31-22-76.us-east-2.compute.internal>
2018-07-08 14:38:58 +0000

    Finish user index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
+ before_action :authenticate_user!, :except=>[:show]
+ 
+ def index
+  @users = User.paginate(page: params[:page])
+ end
+    
+ def show
+  @user = User.find(params[:id])
+ end
 end
