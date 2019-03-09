a00b7ca84236bec2f129ad8bb0ccd41b0bcfdc37
EC2 Default User <ec2-user@ip-172-31-22-76.us-east-2.compute.internal>
2019-03-09 22:53:11 +0000

    teisyutu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # 3.10 tuikasitayo
   protect_from_forgery with: :exception
 end
